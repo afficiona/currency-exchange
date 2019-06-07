afc27cf4ac764e82a61c7471c6e370cc4af275cd
Aman Pandey <aman.pandey@rksv.in>
2019-06-07 21:15:30 +0530

    fix source and target in api

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ import { EXCHANGE_RATE_FETCH_API } from './../constants';
  * API to fetch the customers list.
  * If the response is not 200(ok), throw an error with the message. Else, return the response.
  */
-export const fetchExchangeRateApi = (from, to) => 
+export const fetchExchangeRateApi = (source, target) => 
   fetch(`${EXCHANGE_RATE_FETCH_API}?base=${source}&symbols=${target}`)
     .then(res => {
       
